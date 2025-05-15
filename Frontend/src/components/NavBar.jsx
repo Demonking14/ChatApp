1adf31620060478b20ad698699fad07ff494cc0c
Demonking14 <adi.jha1424@gmail.com>
2025-05-15 11:43:57 +0530

    fix: Update branding in NavBar component from Streamify to ChatyFi

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -21,7 +21,7 @@ function NavBar() {
               <Link to="/" className="flex items-center gap-2.5">
                 <ShipWheelIcon className="size-9 text-primary" />
                 <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  Streamify
+                  ChatyFi
                 </span>
               </Link>
             </div>
